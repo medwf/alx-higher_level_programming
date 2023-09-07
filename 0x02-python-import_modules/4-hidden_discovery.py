180968a4b9db2432c2a11b2392b1a209ce179990
med.wf <med.wf95@gmail.com>
2023-09-07 13:43:55 +0100

    add task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    for i in range(len(dir(hidden_4))):
-        if (dir(hidden_4)[i][:2] == "__"):
-            continue
-        print(dir(hidden_4)[i])
+    import importlib.util
+    from inspect import getmembers, isfunction
+    spec = importlib.util.spec_from_file_location("hidden", "hidden_4.pyc")
+    h = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(h)
+    for name, obj in getmembers(h):
+        if isfunction(obj) and name != "__init__":
+            print("{}".format(name))
