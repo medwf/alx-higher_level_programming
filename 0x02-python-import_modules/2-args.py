51c9ecf78d382a733a13e1a41ce524247c629645
med.wf <med.wf95@gmail.com>
2023-09-07 12:44:46 +0100

    change task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
-    print("0 arguments.")
-else:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-for i in range(1, len(sys.argv)):
-    print("{:d}: {:s}".format(i, sys.argv[i]))
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
