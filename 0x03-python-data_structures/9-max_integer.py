475f337780d77d24189cf18e699370f3445cc100
med.wf <med.wf95@gmail.com>
2023-09-09 19:09:50 +0100

    try to fix task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list != None:
+    if my_list is not None:
         num = 0
         for idx in my_list:
             if num < idx:
                 num = idx
         return num
+    return None
