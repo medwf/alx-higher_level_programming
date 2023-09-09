84c74a5441db2fcbf4549b89c218c7a76858ac9c
med.wf <med.wf95@gmail.com>
2023-09-09 19:18:01 +0100

    fixed error if len = 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
-        return None
+    if my_list is None or len(my_list) == 0:
+        return
     else:
         num = my_list[0]
         for idx in my_list:
