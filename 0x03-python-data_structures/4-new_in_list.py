1941e695eb8fd7ecf86b91c682eb5e218f042474
med.wf <med.wf95@gmail.com>
2023-09-09 18:50:59 +0100

    fixed task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
+    if my_list is not None and 0 <= idx < len(my_list):
         new_list = my_list.copy()
-        for i in range(0, len(my_list)):
-            if i == idx:
-                new_list[i] = element
-            else:
-                new_list[i] = my_list[i]
-    return new_list
+        new_list[idx] = element
+        return new_list
