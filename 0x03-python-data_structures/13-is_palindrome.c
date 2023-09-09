e1153c1df80829ee4f563b907b5cc00d08e9a379
med.wf <med.wf95@gmail.com>
2023-09-09 21:10:28 +0100

    add if i have one node

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,36 +8,36 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *check = *head;
-	int i = 0, len = 0, first = 0, second = 0;
+    listint_t *check = *head;
+    int i = 0, len = 0, first = 0, second = 0;
 
-	if (!*head || !*head->next)
-		return (1);
-	while (check)
-		check = check->next, len++;
-	check = *head;
-	/* printf("*---> %d\n", len); */
+    if (!*head || !(*head)->next)
+        return (1);
+    while (check)
+        check = check->next, len++;
+    check = *head;
+    /* printf("*---> %d\n", len); */
 
-	while (check)
-	{
-		if (len % 2 == 0)
-		{
-			if (i < (len / 2))
-				first += check->n;
-			else
-				second += check->n;
-		}
-		else
-		{
-			if (i <= (len / 2))
-				first += check->n;
-			if (i >= (len / 2))
-				second += check->n;
-		}
-		check = check->next, i++;
-	}
-	/* printf("first = %d \nsecond = %d \n", first, second);*/
-	if (first == second)
-		return (1);
-	return (0);
+    while (check)
+    {
+        if (len % 2 == 0)
+        {
+            if (i < (len / 2))
+                first += check->n;
+            else
+                second += check->n;
+        }
+        else
+        {
+            if (i <= (len / 2))
+                first += check->n;
+            if (i >= (len / 2))
+                second += check->n;
+        }
+        check = check->next, i++;
+    }
+    /* printf("first = %d \nsecond = %d \n", first, second);*/
+    if (first == second)
+        return (1);
+    return (0);
 }
