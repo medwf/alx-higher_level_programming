60841edc84f763b306b24959693ddf58743d0dc4
med.wf <med.wf95@gmail.com>
2023-09-11 17:17:50 +0100

    make some change

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,21 +9,20 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *helper = *head;
-	int array[1024], start, end;
+	int array[2048], start, end;
 
-	if (!*head)
-		return (1);
-
-	for (end = 0; helper; end++)
-		array[end] = helper->n, helper = helper->next;
-
-	array[end] = '\0';
-	for (start = 0; start < ((end % 2 == 0) ? (end / 2)
-				: (end / 2) + 1); start++)
+	if (*head)
 	{
-		if (array[start] != array[end - start - 1])
-			return (0);
-		/*printf("start = %d - end = %d\n", array[start],array[end- start - 1]);*/
+		for (end = 0; helper; end++)
+			array[end] = helper->n, helper = helper->next;
+		array[end] = '\0';
+		for (start = 0; start < end / 2; start++)
+		{
+			/* ignore middle node if it even*/
+			if (array[start] != array[end - start - 1])
+				return (0);
+			/*printf("start = %d-end = %d\n", array[start], array[end - start - 1]);*/
+		}
 	}
 	return (1);
 }
