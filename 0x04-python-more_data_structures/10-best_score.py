8c914a12047d0c8651f04dd829364dc676a74670
med.wf <med.wf95@gmail.com>
2023-09-13 19:43:52 +0100

    fixed task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary) != 0:
         big, key = 0, ""
         for k, i in a_dictionary.items():
             if big < i:
                 big, key = i, k
-                # print(big, key)
         return key
     return None
 
