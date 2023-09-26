cd967db0825cf4d71bf2fa53284dfd26a8b72544
med.wf <med.wf95@gmail.com>
2023-09-26 21:51:26 +0100

    add task 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,8 +3,6 @@
 
 
 class Node:
-    """  This class allows you to create and manipulate SinglyLinkedList objects """
-
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
@@ -16,17 +14,17 @@ class Node:
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
-            raise TypeError('data must be an integer')
+            raise TypeError("data must be an integer")
         self._data = value
 
     @property
     def next_node(self):
         return self._next_node
 
-    @data.setter
+    @next_node.setter
     def next_node(self, value):
         if value is not None and not isinstance(value, Node):
-            raise TypeError('next_node must be a Node object')
+            raise TypeError("next_node must be a Node object")
         self._next_node = value
 
 
@@ -36,25 +34,26 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         new_node = Node(value)
-
         if self.head is None:
             self.head = new_node
+            return
 
-        elif value < self.head.data:
+        if value < self.head.data:
             new_node.next_node = self.head
             self.head = new_node
-        else:
-            current = self.head
-            while current.next_node is not None and current.next_node.data < value:
-                current = current.next_node
+            return
 
-            new_node.next_node = current.next_node
-            current.next_node = new_node
+        current = self.head
+        while current.next_node is not None and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
 
     def __repr__(self):
         node = self.head
         nodes = []
         while node is not None:
-            nodes.append(int(node.data))
+            nodes.append(str(node.data))
             node = node.next_node
         return "\n".join(nodes)
