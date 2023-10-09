0f63ab0c11271536078876ab7581460a25a7c3d8
med.wf <med.wf95@gmail.com>
2023-10-09 22:18:43 +0100

    add some change

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains a class that defines a BaseGeometry.
+This module contains a class that defines a rectangle.
 """
 
 
@@ -14,8 +14,10 @@ class BaseGeometry:
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """ validation value """
+        """Validation value"""
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            str_err = "{:s} must be an integer".format(name)
+            raise TypeError(str_err)
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            str_err = "{:s} must be greater than 0".format(name)
+            raise ValueError(str_err)
