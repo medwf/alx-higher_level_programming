5368fb8bf9c80fa55e9ed354b97c5aea0da6b20f
med.wf <med.wf95@gmail.com>
2023-10-09 22:37:41 +0100

    add task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module contains a class that defines a rectangle.
+"""
+
+
+class BaseGeometry:
+    """Defines a BaseGeometry"""
+
+    def area(self):
+        """Method: raises an Exception with the message
+            area() is not implemented
+        """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validation value"""
+        if not isinstance(value, int):
+            str_err = "{:s} must be an integer".format(name)
+            raise TypeError(str_err)
+        if value <= 0:
+            str_err = "{:s} must be greater than 0".format(name)
+            raise ValueError(str_err)
+
+
+class Rectangle(BaseGeometry):
+    """Defines a Rectangle"""
+
+    def __init__(self, width, height):
+        """ initialized atribute """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
