8f879dc7ad17ef75e9650bd156d054260e4c4604
med.wf <med.wf95@gmail.com>
2023-10-09 15:53:26 +0100

    fix error

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,10 +7,6 @@ Mylist is a class inherits_ list
 class MyList(list):
     """subclass of list"""
 
-    def __init__(self):
-        """initisalizes the object"""
-        super().__init__()
-
     def print_sorted(self):
         """public methods print sorted list"""
         print(sorted(self))
