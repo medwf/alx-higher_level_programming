16dcdf08b9faf8be1071e89cde185bf4566376e8
med.wf <med.wf95@gmail.com>
2023-10-10 23:38:37 +0100

    add task another sulotion

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,6 +2,18 @@
 """pascal_triange module"""
 
 
+def fac(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fac(n - 1)
+
+
+def combinations(n, k):
+    a, b, c = fac(n), fac(k), fac(n - k)
+    return int(a / (b * c))
+
+
 def pascal_triangle(n):
     """
     Args:
@@ -16,21 +28,29 @@ def pascal_triangle(n):
     lst = []
     # start loop from 0 to n - 1
     for i in range(n):
-        # i use 11 ** i eash line
-        """
-        for n == 5
-        line 1 = 11 ** 0 = 1
-        line 2 = 11 ** 1 = 11
-        line 3 = 11 ** 2 = 121
-        line 4 = 11 ** 3 = 1331
-        line 5 = 11 ** 4 = 14641
-        """
-        number = 11 ** i
-        # test number is print("number = ", number)
-        # change number to string to take each num from begginng.
-        # casting char to int and make list
-        _l = [int(digit) for digit in str(number)]
-        # test list inside print("inside list = ", _l)
+        # # i use 11 ** i eash line
+        # """
+        # for n == 5
+        # line 1 = 11 ** 0 = 1
+        # line 2 = 11 ** 1 = 11
+        # line 3 = 11 ** 2 = 121
+        # line 4 = 11 ** 3 = 1331
+        # line 5 = 11 ** 4 = 14641
+        # """
+        # number = 11 ** i
+        # # test number is
+        # print("number = ", number)
+        # # change number to string to take each num from begginng.
+        # # casting char to int and make list
+        # _l = [int(digit) for digit in str(number)]
+        # # test list inside print("inside list = ", _l)
+        # lst.append(_l)
+        # # test list of list print("all list = ", lst)
+        i2 = i
+        _l = []
+        for k in range(i2 + 1):
+            print(combinations(i, k))
+            _l.append(combinations(i, k))
         lst.append(_l)
-        # test list of list print("all list = ", lst)
+
     return lst
