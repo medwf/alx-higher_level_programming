58378d04e85e5b155bc863c84f10378183081312
med.wf <med.wf95@gmail.com>
2023-10-11 15:33:16 +0100

    finally add task 100

---
+++ 0x0B-python-input_output/100-main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+append_after = __import__('100-append_after').append_after
+
+append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+
+
+"""
+At Holberton School,
+Python is really important,
+But it can be very hard if:
+- You don't get all Pythonic tricks
+- You don't have strong C knowledge.
+
+"""
+"""
+At School,
+Python is really important,
+"C is fun!"
+But it can be very hard if:
+- You don't get all Pythonic tricks
+"C is fun!"
+- You don't have strong C knowledge.
+
+At School,
+Python is really important,
+"C is fun!"
+"C is fun!"
+But it can be very hard if:
+- You don't get all Pythonic tricks
+"C is fun!"
+"C is fun!"
+- You don't have strong C knowledge.
+"""
