d5ca64451b312c68d2564d15b3985e15e49fe861
med.wf <med.wf95@gmail.com>
2023-11-14 18:43:10 +0100

    add task 19

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+--  cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table WHERE name is not NULL ORDER BY score DESC;
