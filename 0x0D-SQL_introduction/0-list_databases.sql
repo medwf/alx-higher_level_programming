1739b14b9eaf9988ad7562c84b0d5aa7a4d740f4
med.wf <med.wf95@gmail.com>
2023-11-14 14:16:27 +0100

    start project SQL - Introduction

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all databases of your MySQL server.
+-- cat 0-list_databases.sql | sudo mysql -hlocalhost -uroot -p
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Write a script that creates the database hbtn_0c_0 in your MySQL server.
+-- cat 1-create_database_if_missing.sql | mysql -hlocalhost -uroot -p
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
