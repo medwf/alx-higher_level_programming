8d4464cc0def6b2969950f697fbf3809bab5a69b
med.wf <med.wf95@gmail.com>
2023-11-15 17:45:57 +0100

    add task 5

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Write a script that creates the table unique_id on your MySQL server.
+-- cat 5-unique_id.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
