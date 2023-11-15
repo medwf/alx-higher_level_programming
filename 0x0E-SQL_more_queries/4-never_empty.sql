fae014cb06088504a28cf83b1c7c74c4dc452f43
med.wf <med.wf95@gmail.com>
2023-11-15 17:39:45 +0100

    fix id default by 1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,6 @@
 -- Write a script that creates the table id_not_null on your MySQL server.
 -- cat 4-never_empty.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT PRIMARY KEY,
+    id INT DEFAULT 1,
     name VARCHAR(256)
 );
