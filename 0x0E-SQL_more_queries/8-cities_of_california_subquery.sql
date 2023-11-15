1a9473639fc2c56ced5544c62ada43ac15e303a3
med.wf <med.wf95@gmail.com>
2023-11-15 19:11:04 +0100

    add task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- cat 8-cities_of_california_subquery.sql | mysql -hlocalhost -uroot -p hbtn_0d_usa
+SELECT id, name FROM cities
+WHERE state_id IN (SELECT id FROM states WHERE name = "California") ORDER BY id;
