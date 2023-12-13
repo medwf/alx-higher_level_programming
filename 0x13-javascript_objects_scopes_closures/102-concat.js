3c48a376235dc7eb9bd488a424c78a0bad2894b6
med.wf <med.wf95@gmail.com>
2023-12-13 17:03:44 +0100

    add last task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 const Args = process.argv.slice(2);
-const fs = require('fs').promises;
-// console.log(Args);
+const fs = require('fs');
 
-const f1 = Args[0];
-const f2 = Args[1];
-const f3 = Args[2];
-
-// console.log(f1, f2, f3);
-try {
-  const d1 = fs.readFile(f1);
-  console.log(d1);
-} catch (err) {
-  console.log(err);
+if (Args.length === 3) {
+  const fileA = Args[0];
+  const fileB = Args[1];
+  const fileC = Args[2];
+  /* console.log(fileA, fileB, fileC); */
+  const fileAcontent = fs.readFileSync(fileA, 'utf8');
+  const fileBcontent = fs.readFileSync(fileB, 'utf8');
+  const concFile = fileAcontent + fileBcontent;
+  fs.writeFileSync(fileC, concFile, 'utf8');
 }
