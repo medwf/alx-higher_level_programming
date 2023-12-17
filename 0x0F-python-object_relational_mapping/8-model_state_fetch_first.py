5b94806ed87b7743da1b9b7a6340fa919e0d3c4f
med.wf <med.wf95@gmail.com>
2023-12-17 14:28:07 +0100

    just one state add task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    states = session.query(State).order_by(State.id).all()
+    states = session.query(State).order_by(State.id).first()
     if len(states):
         print("{}: {}".format(states[0].id, states[0].name))
     else:
