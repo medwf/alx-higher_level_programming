f859f728eab55c55eb038f9108d131657cfcddd7
med.wf <med.wf95@gmail.com>
2023-12-17 18:50:48 +0100

    task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,14 +7,14 @@ from model_city import City
 import sys
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    # cities = session.query(City).join(State).order_by(City.id).all()
-    cities = session.query(City, State).\
-        join(State, State.id == City.state_id).all()
-    if cities:
-        for city, state in cities:
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
+    for state, city in session.query(State, City).\
+            filter(State.id == City.state_id).order_by(City.id):
+        print('{}: ({}) {}'.format(state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""import model"""
-
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+"""This is the City module.
+Contains the City class that inherits from Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
 from model_state import Base
 
 
 class City(Base):
-    """ DOC """
+    """This class links to the `cities` table of our database.
+    Attributes:
+        id (int): id of the city.
+        name (str): name of the city.
+        state_id (int): id of the associated state.
+    """
+
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    state = relationship('State', foreign_keys=[state_id])
