e009462f6d69e4d35bc56a1e27b5cac5295ae3c6
med.wf <med.wf95@gmail.com>
2023-12-17 15:59:30 +0100

    add task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session = Session(engine)
+    StateName = "Louisiana"
+    session.add(State(name=StateName))
+    state = session.query(State).filter(State.name == StateName).first()
+    print("{}".format(state.id))
+    session.close()
