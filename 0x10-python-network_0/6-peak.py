cc901d738be35288de32bf992f2c04ea92c35c15
med.wf <med.wf95@gmail.com>
2024-01-05 21:14:43 +0100

    add task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers."""
+    if len(list_of_integers) == 0:
+        return None
+    return sorted(list_of_integers)[-1]
