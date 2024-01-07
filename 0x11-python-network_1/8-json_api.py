f5fcbca1af64c6c3ab84c849cf2db979fc2ceb26
med.wf <med.wf95@gmail.com>
2024-01-07 21:45:37 +0100

    add tasks

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""import model"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    data = {"q": argv[1] if len(argv) > 1 else ""}
+    request = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        json = request.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
