cc768e88987b8d71a36f0cf46640a46a0c190622
med.wf <med.wf95@gmail.com>
2024-01-30 16:51:16 +0100

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length > 1) {
+  const id = process.argv[2];
+  const URL = 'https://swapi-api.alx-tools.com/api/films/' + id;
+  request(URL, (error, responde, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(JSON.parse(responde.body).title);
+    }
+  });
+}
