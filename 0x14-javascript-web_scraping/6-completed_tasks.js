3c149969850a961339117aee9a91b10f475dbc4d
med.wf <med.wf95@gmail.com>
2024-01-30 19:55:21 +0100

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length > 2) {
+  const URL = process.argv[2];
+  request(URL, (error, res, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res = JSON.parse(body);
+      const data = {};
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].completed && data[res[i].userId] === undefined) {
+          data[res[i].userId] = 1;
+        } else if (res[i].completed) {
+          data[res[i].userId] += 1;
+        }
+      }
+      console.log(data);
+    }
+  });
+}
