cfd51c46c02b2129ba1c5d7987aa1271efad588a
med.wf <med.wf95@gmail.com>
2024-01-30 18:09:12 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length > 2) {
+  const URL = process.argv[2];
+  request(URL, (error, responde, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const results = JSON.parse(body).results;
+      let res = 0;
+      // loop for every movie
+      for (let movies = 0; movies < results.length; movies++) {
+        // loop for every char
+        for (let char = 0; char < results[movies].characters.length; char++) {
+          if (results[movies].characters[char].endsWith('/18/')) {
+            res++;
+          }
+        }
+      }
+      console.log(res);
+    }
+  });
+}
