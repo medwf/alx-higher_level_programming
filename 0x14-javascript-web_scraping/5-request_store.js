d1dabe60288d298bda90ae861a63eaa452af0eee
med.wf <med.wf95@gmail.com>
2024-01-30 18:47:33 +0100

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length > 3) {
+  const URL = process.argv[2];
+  const file = process.argv[3];
+
+  request(URL, (error, response, body) => {
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+    if (error) {
+      console.error(error);
+    }
+  });
+}
