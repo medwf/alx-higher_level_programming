c2a32dabea5ad0352bbf930663f1e4a693bff8d9
med.wf <med.wf95@gmail.com>
2024-01-30 16:09:51 +0100

    add task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length > 1) {
+  const URL = process.argv[2];
+  request(URL, (error, responce, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('code:', responce.statusCode);
+    }
+  });
+}
